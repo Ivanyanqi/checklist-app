97ab55687a70acf72d51701faa2bccb7fcc6091d
yanqi12 <yanqi12@meituan.com>
2026-05-20 14:35:01 +0800

    Initial Commit

---
+++ ChecklistApp/ChecklistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistAppApp.swift
+//  ChecklistApp
+//
+//  Created by yanqi on 2026/5/20.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChecklistApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChecklistApp
+//
+//  Created by yanqi on 2026/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
